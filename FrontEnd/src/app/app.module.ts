800bf74b62b30594ba2d386ffc2b33e1f2bc3c19
Rohit Luthra <rohitluthra9@outlook.com>
2019-10-28 16:09:47 -0400

    Login Page Done, I guess

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { StuDashComponent } from './stu-dash/stu-dash.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    StuDashComponent
   ],
   imports: [
     BrowserModule
