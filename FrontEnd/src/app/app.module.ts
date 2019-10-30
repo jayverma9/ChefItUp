8893f00b403d1239e2c3c874538ee0cfb6b2b2d3
Rohit Luthra <rohitluthra9@outlook.com>
2019-10-30 03:03:37 -0400

    Login/Student/Instructor Page Done.

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { StuDashComponent } from './stu-dash/stu-dash.component';
+import { InstructorComponent } from './instructor/instructor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    StuDashComponent
+    StuDashComponent,
+    InstructorComponent
   ],
   imports: [
     BrowserModule
