75b3ca295357cb3b2c4c370daab831219b5dcc58
Kishore Thamilvanan <kishore.thamilvanan@stonybrook.edu>
2019-11-11 18:05:46 -0500

    Test commit before merging RS branch.

--- src/main/java/purple/Configuration/ClassMongoDBConfig.java
+++ src/main/java/purple/Configuration/ClassMongoDBConfig.java
@@ -28,13 +28,21 @@ public class ClassMongoDBConfig {
 
 //            classRepository.save(new Class("Greek Food", "Chicken Gyro",  new Recipe[0], new Student[0]));
 //            ingredientRepository.save(new Ingredient("parsley", "Empty", "leafy vegetable", "solid", "smells good"));
-//            recipeRepository.save(new Recipe("Paneer Tadka", "abhi toh bola paneer tadka", new Ingredient[0], new Utensil[0], new String[0], new Student[0]));
-//            studentRepository.save(new Student("KISH", "123", "Kishore Thamilvanan"));
+//            recipeRepository.save(new Recipe("Paneer Tadka", "description", ingredientRepository.findAll(), utensilRepository.findAll(), new String[0], studentRepository.findAll()));
+//            recipeRepository.save(new Recipe("Spaghetti", "description", ingredientRepository.findAll(), utensilRepository.findAll(), new String[0], studentRepository.findAll()));
+//            recipeRepository.save(new Recipe("Chicken masala", "description", ingredientRepository.findAll(), utensilRepository.findAll(), new String[0], studentRepository.findAll()));
+//            recipeRepository.save(new Recipe("Goat Masala", "description", ingredientRepository.findAll(), utensilRepository.findAll(), new String[0], studentRepository.findAll()));
+//
+//            studentRepository.save(new Student("jack", "123", "Percy Jackson", classRepository.findAll(), "looking for Water recipes", recipeRepository.findAll()));
+//            studentRepository.save(new Student("john", "123", "John Wick", classRepository.findAll(), "looking for Blood recipes", recipeRepository.findAll()));
+//            studentRepository.save(new Student("stup", "123", "Stupinder", classRepository.findAll(), "looking for idiot recipes", recipeRepository.findAll()));
+//            studentRepository.save(new Student("devil", "123", "Devil ka bacha", classRepository.findAll(), "looking for firey recipes", recipeRepository.findAll()));
+
 //            teacherRepository.save(new Teacher("ROHIT", "abc", "ROHIT SINGH THARDAK", new Class[0], "", new Recipe[0], new Recipe[0]));
 //            teacherRepository.save(new Teacher("Rluthra", "123", "Rohit Luthra", new Class[0], "", new Recipe[0], new Recipe[0]));
 //            utensilRepository.save(new Utensil("pan", "Empty", "3/4 quarts", new String[0], "Cast Iron Pan"));
 
-            System.out.println(classRepository.findAll());
+//            System.out.println(classRepository.findAll());
 
             teacherRepository.save(
                     new Teacher(
@@ -47,6 +55,31 @@ public class ClassMongoDBConfig {
                             recipeRepository.findAll()
                     )
             );
+
+
+//            classRepository.save(
+//
+//                    new Class("Indian Food",
+//                            "Spicy and burst of flavours",
+//                            recipeRepository.findAll(),
+//                            studentRepository.findAll()
+//                    )
+//                );
+//            classRepository.save(
+//
+//                    new Class("Italian Food",
+//                            "Spicy and burst of flavours",
+//                            recipeRepository.findAll(),
+//                            studentRepository.findAll()
+//                    )
+//            ); classRepository.save(
+//
+//                    new Class("Jamaican Food",
+//                            "Spicy and burst of flavours",
+//                            recipeRepository.findAll(),
+//                            studentRepository.findAll()
+//                    )
+//            );
         };
     }
 }
